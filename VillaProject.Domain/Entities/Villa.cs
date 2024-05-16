9536cb5d27c368d34aa4cd62c949565aca24f783
MarwanMohammed <marwan.m.rabea@gmail.com>
2024-05-16 05:30:43 +0300

    Create Villa Model Entity

---
+++ VillaProject.Domain/Entities/Villa.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Villa
+{
+    [Key]
+    public int ID { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string VillaName { get; set; } = null!;
+    [StringLength(250)]
+    public string? Description { get; set; }
+    [Column(TypeName = "money")]
+    public double Price { get; set; }
+    public int Occupancy { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
