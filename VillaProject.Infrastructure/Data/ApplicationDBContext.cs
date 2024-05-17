7330a6543cff60c68e9163366a011ef23c681642
MarwanMohammed <marwan.m.rabea@gmail.com>
2024-05-18 01:26:32 +0300

    Add New Migrations

--- VillaProject.Infrastructure/Data/ApplicationDBContext.cs
+++ VillaProject.Infrastructure/Data/ApplicationDBContext.cs
@@ -1,7 +1,49 @@
+using System.Data;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDBContext : DbContext
 {
     public DbSet<Villa> Villas { get; set; }
     public ApplicationDBContext(DbContextOptions options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Villa>().HasData(
+            new Villa
+            {
+                ID = 1,
+                VillaName = "VillaNameOne",
+                Price = 19000.50M,
+                Occupancy = 4,
+                ImageUrl = "Https://placeholder.com",
+                Description = "VillaNameOne and 4 rooms and bathroom",
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.MinValue
+            }
+    ,
+      new Villa
+      {
+          ID = 2,
+          VillaName = "VillaNameTwo",
+          Price = 2500.50M,
+          Occupancy = 1,
+          ImageUrl = "Https://placeholder.com",
+          Description = "VillaNameTwo and 1 rooms",
+          CreatedDate = DateTime.Now,
+          UpdatedDate = DateTime.MinValue
+      }
+    ,
+       new Villa
+       {
+           ID = 3,
+           VillaName = "VillaNameTwo",
+           Price = 5000.57M,
+           Occupancy = 2,
+           ImageUrl = "Https://placeholder.com",
+           Description = "VillaNameThree and 2 rooms",
+           CreatedDate = DateTime.Now,
+           UpdatedDate = DateTime.MinValue
+       }
+        );
+    }
+
 }
--- VillaProject.Infrastructure/Migrations/20240517222202_InitSecondUpdate.Designer.cs
+++ VillaProject.Infrastructure/Migrations/20240517222202_InitSecondUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace VillaProject.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240517222202_InitSecondUpdate")]
+    partial class InitSecondUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
