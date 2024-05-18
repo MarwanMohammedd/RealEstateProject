1a34c1a7efac3c1125f6c4011a06ecd293f62778
MarwanMohammed <marwan.m.rabea@gmail.com>
2024-05-18 03:20:28 +0300

    Add Villa Controller

---
+++ VillaProject.Presentation/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+/// <summary>
+/// Controller for Villa Entity.
+/// </summary>
+public class VillaController : Controller
+{
+    private readonly ILogger<VillaController> _logger;
+    private readonly ApplicationDBContext _applicationDBContext;
+    public VillaController(ApplicationDBContext _applicationDBContext, ILogger<VillaController> _logger)
+    {
+        this._applicationDBContext = _applicationDBContext;
+        this._logger = _logger;
+    }
+
+    //Defualt EndPoint Of Villa Controller
+    public IActionResult Index(){
+        _logger.LogInformation("InSide Index Action Method");
+        var result = _applicationDBContext.Villas.ToList();
+        return View(result);
+    }
+}
